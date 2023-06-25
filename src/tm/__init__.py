b71681f374d734d9ebca481f229907e49bb0f6e7
Ross Boczar <rjboczar@gmail.com>
2023-06-25 07:55:42 -0700

    better logging message

--- src/tm/__init__.py
+++ src/tm/__init__.py
@@ -6,6 +6,8 @@ logger.setLevel(logging.INFO)
 ch = logging.StreamHandler(stream=sys.stdout)
 ch.setLevel(logging.INFO)
 
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s [%(name)s.%(levelname)s]: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
