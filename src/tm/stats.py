28a95c75480b865491592f46af5bbe29a97f73a2
Ross Boczar <rjboczar@gmail.com>
2023-06-26 08:38:02 -0700

    cleaning up some map stats

--- src/tm/stats.py
+++ src/tm/stats.py
@@ -1,46 +1,5 @@
+import numpy as np
 import pandas as pd
-
-
-def _untied(points_d: dict[str, int]) -> bool:
-    """
-    Returns True if the map is untied w.r.t. seconds digit.
-    :param points_d: dict mapping usernames to points.
-    :return: bool
-    """
-    # points_d is already sorted, so just check if there aren't two 3s
-    return (len(points_d) > 1) and (list(points_d.values())[1] != 3)
-
-
-def single_map_stats(group_df: pd.DataFrame) -> pd.Series:
-    """
-    Computes map stats for a single map.
-
-    :param group_df: DataFrame of map records for a single map, as computed in pd.groupby().
-    :return: Series of map stats.
-    """
-    n = len(group_df)
-    multi_user = n > 1
-    best = group_df.iloc[0]
-    second_best = group_df.iloc[1] if n > 1 else None
-    stats_ = pd.Series(
-        {
-            **best,
-            # Second-fastest user (else '')
-            "second_user": second_best["username"] if multi_user else "",
-            # Gap between best and second-best times (else pd.NA)
-            "gap": second_best["record_time"] - best["record_time"]
-            if multi_user
-            else pd.NA,
-            # Whether more than one user has played the track
-            "multi_user": multi_user,
-        }
-    ).rename({"record_time": "best_time"})
-    # Whether the map is 'untied' w.r.t. seconds digit
-    stats_["untied"] = not multi_user or (
-        group_df["points"].iloc[0] > group_df["points"].iloc[1]
-    )
-    stats_["points_str"] = ",".join(group_df["points"].astype(str))
-    return stats_
 
 
 def map_stats(df: pd.DataFrame) -> pd.DataFrame:
@@ -61,6 +20,35 @@ def map_stats(df: pd.DataFrame) -> pd.DataFrame:
     int_cols = ["best_time", "record_medal", "gap", "points"]
     df[int_cols] = df[int_cols].astype("Int64")
     return df
+
+
+def single_map_stats(group_df: pd.DataFrame) -> pd.Series:
+    """
+    Computes map stats for a single map.
+
+    :param group_df: DataFrame of map records for a single map, as computed in pd.groupby().
+    :return: Series of map stats.
+    """
+    multi_user = len(group_df) > 1
+    best = group_df.iloc[0]
+    second_best = group_df.iloc[1] if multi_user else None
+    stats_ = pd.Series(
+        {
+            **best,
+            # Second-fastest user (else ")
+            "second_user": second_best["username"] if multi_user else "",
+            # Gap between best and second-best times (else pd.NA)
+            "gap": second_best["record_time"] - best["record_time"]
+            if multi_user
+            else pd.NA,
+            # Whether more than one user has played the track
+            "multi_user": multi_user,
+        }
+    ).rename({"record_time": "best_time"})
+    # Whether the map is 'untied' w.r.t. seconds digit
+    stats_["untied"] = np.diff(group_df["points"], append=0)[0] < 0
+    stats_["points_str"] = ",".join(group_df["points"].astype(str))
+    return stats_
 
 
 def map_points(group_df: pd.DataFrame) -> pd.DataFrame:
